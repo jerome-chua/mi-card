ea197675cc2bb5766dea029efe6553b29797b80e
Jchu4 <jerome.chuame@gmail.com>
2021-08-27 16:22:56 +0800

    Update text and font

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-      MyApp()
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,28 +9,62 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.teal,
-          body: SafeArea(
-            child: Column(
-              children: <Widget>[
-                Center(
-                  child: CircleAvatar(
-                  radius: 50.0q,
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  radius: 50.0,
                   backgroundImage: AssetImage('images/jerome.jpg'),
-                  ),
                 ),
-                Text(
-                    'Jerome',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0,
+              ),
+              Text(
+                'Jerome',
+                style: TextStyle(
+                  fontFamily: 'Allura',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30.0,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'SOFTWARE ENGINEER',
+                style: TextStyle(
+                  fontFamily: 'SourceSans',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.mail_outline,
+                      color: Colors.teal,
                     ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'jerome.chuame@gmail.com',
+                      style: TextStyle(
+                          fontFamily: 'SourceSans',
+                          fontSize: 18.0,
+                          color: Colors.teal.shade900),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              )
+            ],
           ),
         ),
-      );
+      ),
+    );
   }
 }
-
